f6da4f37d9692819fa0666e2b3f92c473c013dd3
lamar2013 <lamararmaly2102@gmail.com>
2019-09-25 12:01:22 +0300

    initial

--- app/src/main/java/y2019/aoc/lamar/lamaraoc2019/HomePageActivity.java
+++ app/src/main/java/y2019/aoc/lamar/lamaraoc2019/HomePageActivity.java
@@ -2,6 +2,8 @@ package y2019.aoc.lamar.lamaraoc2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page2);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addHomeWork:
+                Toast.makeText(getBaseContext(), "add homework", Toast.LENGTH_LONG).show();
+
+        }
+
+
+
+
     }
 }
