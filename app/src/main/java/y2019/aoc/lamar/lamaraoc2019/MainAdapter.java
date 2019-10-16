e81fd584d04218b54fe6496e49ec9e2a123d95c4
lamar2013 <lamararmaly2102@gmail.com>
2019-10-16 11:10:35 +0300

    initial

---
+++ app/src/main/java/y2019/aoc/lamar/lamaraoc2019/MainAdapter.java
@@ -0,0 +1,46 @@
+package y2019.aoc.lamar.lamaraoc2019;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class MainAdapter extends BaseAdapter {
+
+    private Context context;
+    private LayoutInflater inflater;
+    private String[] dayWord;
+
+    public MainAdapter(Context context, String dayWord){
+        this.context=context;
+        this.dayWord=dayWord;
+    }
+
+
+    @Override
+    public int getCount() {
+        return dayWord.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(inflater==null){
+            inflater=(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)
+        }
+        if(convertView==null){
+            convertView = inflater.inflate(R.layout.row_item,null);
+        }
+        return null;
+    }
+}
--- app/src/main/java/y2019/aoc/lamar/lamaraoc2019/ScheduleActivity.java
+++ app/src/main/java/y2019/aoc/lamar/lamaraoc2019/ScheduleActivity.java
@@ -2,18 +2,34 @@ package y2019.aoc.lamar.lamaraoc2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class ScheduleActivity extends AppCompatActivity {
 
-    //GridView gridView;
+    GridView gridView;
+
+    String[] dayWord= {"Monday","Tuesday","Wednesday","Thursday","Friday"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
 
-        //gridView= (GridView) findViewById(R.id.GridView);ظظ
+        gridView= findViewById(R.id.gridView);
+
+        MainAdapter adapter= new MainAdapter(ScheduleActivity.this,dayWord);
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "you clicked"+dayWord[+position],Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
