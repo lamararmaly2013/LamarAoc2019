2148ffea7071110a0971ef111fd477e96832754f
lamar2013 <lamararmaly2102@gmail.com>
2019-11-08 09:17:11 +0200

    initial

--- app/src/main/java/y2019/aoc/lamar/lamaraoc2019/HomeWorkActivity.java
+++ app/src/main/java/y2019/aoc/lamar/lamaraoc2019/HomeWorkActivity.java
@@ -1,6 +1,8 @@
 package y2019.aoc.lamar.lamaraoc2019;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,13 +10,28 @@ import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
-public class HomeWorkActivity extends AppCompatActivity {
+public class HomeWorkActivity extends AppCompatActivity{
 
     ArrayList<HomeWork> homeWorks;
     ListView hwlv;
 
+
+//*
+    final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    final DatabaseReference myRef = database.getReference("Users/"+user.getUid()+"/HW");
+//*
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +39,47 @@ public class HomeWorkActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home_work);
 
         homeWorks = new ArrayList<>();
-        homeWorks.add(new HomeWork("CS", "Java HW", null, false));
-        homeWorks.add(new HomeWork("Arabic", "insha HW", null, false));
-        homeWorks.add(new HomeWork("CS", "Java HW", null, false));
+        // homeWorks.add();
+       //* myRef.push().setValue(new HomeWork("CS", "Java HW", null, false));
+        //homeWorks.add(new HomeWork("Arabic", "insha HW", null, false));
+        //homeWorks.add(new HomeWork("CS", "Java HW", null, false));
 
-        homeWorks.add(new HomeWork("CS", "Java HW", null, false));
-        hwlv= findViewById(R.id.HomeWorkListView);
-        CustomAdapter2 adapter2= new CustomAdapter2(this, R.layout.activity_check_box_list, homeWorks);
+        //homeWorks.add(new HomeWork("CS", "Java HW", null, false));
+
+        hwlv = findViewById(R.id.HomeWorkListView);
+        final CustomAdapter2 adapter2= new CustomAdapter2(this, R.layout.activity_check_box_list, homeWorks);
         hwlv.setAdapter(adapter2);
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                HomeWork hw = dataSnapshot.getValue(HomeWork.class);
+                //Toast.makeText(this, "Inside child added", Toast.LENGTH_SHORT).show();
+                homeWorks.add(hw);
+                adapter2.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
     }
 
--- app/src/main/java/y2019/aoc/lamar/lamaraoc2019/LogInActivity.java
+++ app/src/main/java/y2019/aoc/lamar/lamaraoc2019/LogInActivity.java
@@ -1,24 +1,35 @@
 package y2019.aoc.lamar.lamaraoc2019;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "FIREBASE";
     EditText  editTextEmail , editTextPassword;
     Button buttonLogIn , buttonSignUp;
-
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
         editTextEmail=findViewById(R.id.editTextEmail);
         editTextPassword =findViewById(R.id.editTextPassword);
 
@@ -28,9 +39,41 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
         buttonSignUp.setOnClickListener(this);
 
 
-        }
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+    public void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i = new Intent(LogInActivity.this, HomePageActivity.class);
+                            i.putExtra("email", editTextEmail.getText().toString());
+                            i.putExtra("password", editTextPassword.getText().toString());
+                            startActivity(i);
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LogInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
 
+                        // ...
+                    }
+                });
+    }
     @Override
     public void onClick(View v) {
         if(v==buttonLogIn) {
@@ -40,11 +83,8 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
             }
 
             else{
+            signIn(editTextEmail.getText().toString(),editTextPassword.getText().toString());
 
-            Intent i = new Intent(this, HomePageActivity.class);
-            i.putExtra("email", editTextEmail.getText().toString());
-            i.putExtra("password", editTextPassword.getText().toString());
-            startActivity(i);
 
             }
 
